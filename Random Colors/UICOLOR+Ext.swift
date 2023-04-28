900ce947027c3e532064229798761f333ccef7a1
Oleg Plugaru <119738558+OlegPlugaru@users.noreply.github.com>
2023-04-28 15:36:53 +0300

    Initial Commit

---
+++ Random Colors/UICOLOR+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  UICOLOR+Ext.swift
+//  Random Colors
+//
+//  Created by Oleg Plugaru on 31.03.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static func random() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
+    }
+}
